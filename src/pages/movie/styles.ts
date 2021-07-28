6eea6f5092897b4dc8226c4ba81ec4a7afd5a8ad
Manoel Duran <manoel.duran@hotmail.com>
2021-07-27 23:36:54 -0300

    finalizado com bug de loading

--- src/pages/movie/styles.ts
+++ src/pages/movie/styles.ts
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+width:100%;
 `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+font-weight: 500;
+`;
 
-export const Text = styled.p``;
+export const Text = styled.p`
+font-weight: 400;
+`;
